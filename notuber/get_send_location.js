aab4e235988447b671c5d8243d18a7bc0538d8a6
Erika Odmark <erikaodmark@Erikas-Air-10.fios-router.home>
2018-03-05 00:34:54 -0500

    Parse json and display locations of vehicles

--- notuber/get_send_location.js
+++ notuber/get_send_location.js
@@ -11,6 +11,12 @@ var myMarker
 var infowindow = new google.maps.InfoWindow();
 var username = "RG8IotI93V"
 
+// myType is either passenger or person, and dataObjects is either an array
+// of passengers or an array of vehicles (theirType opposite of myType)
+var dataObjects
+var myType
+var theirType
+
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), myOptions);
     getMyLocation();
@@ -22,9 +28,10 @@ function getMyLocation() {
         navigator.geolocation.getCurrentPosition(function(position) {
             myLat = position.coords.latitude;
             myLng = position.coords.longitude;
+            httpPost();
             renderMap();
-            sendInfo();
         });
+
     }
     else {
         alert("Geolocation is not supported by your web browser.");
@@ -35,32 +42,65 @@ function getMyLocation() {
 function renderMap() {
     myCoords = new google.maps.LatLng(myLat, myLng);
     map.panTo(myCoords);
+    // the marker for me
     marker = new google.maps.Marker({
         position: myCoords,
-        title: "<div>"+username+"</div> <div>distance to nearest car: " + "10</div>",
+        title: "<div>" + username + "</div><div>distance to nearest "
+                + theirType + ": " + "10</div>",
     });
     marker.setMap(map);
-
-    // Open info window on click of marker
     google.maps.event.addListener(marker, 'click', function() {
         infowindow.setContent(this.title);
         infowindow.open(map, this);
     });
-}
 
-function sendInfo() {
-    var http = new XMLHttpRequest();
-    var url = "https://jordan-marsh.herokuapp.com/rides"
-    var data = "username=" + username + "&lat=" + myLat + "&lng=" + myLng
-    http.open("POST", url, true)
-
-    http.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
-
-    http.onreadystatechange = func() {
-        if(http.readyState == 4 && http.status == 200) {
-            alert(http.responseText);
-        }
+    //the markers for all other passengers or vehicles
+    for (i = 0; i < dataObjects.length; i++) {
+        var latLng = new google.maps.LatLng(dataObjects[i].lat, dataObjects[i].lng);
+        var usr = dataObjects[i].username;
+        var distToMe = 20
+        m = new google.maps.Marker({
+            position: latLng,
+            title: "<div>" + usr + "</div><div>distance to me: " +
+                    distToMe +  "</div>",
+        });
+        m.setMap(map);
+        google.maps.event.addListener(m, 'click', function() {
+            infowindow.setContent(this.title);
+            infowindow.open(map, this);
+        });
     }
 
-    http.send(data)
+
+}
+
+function httpPost() {
+    var http = new XMLHttpRequest();
+    var url = "https://jordan-marsh.herokuapp.com/rides";
+    var data = "username=" + username + "&lat=" + myLat + "&lng=" + myLng;
+    http.open("POST", url, false);
+
+    http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+    http.onreadystatechange = function() {
+        if(http.readyState == 4 && http.status == 200) {
+            obj = JSON.parse(http.responseText);
+            if(obj.hasOwnProperty('vehicles')){
+                dataObjects = obj.vehicles
+                myType = "passenger"
+                theirType = "vehicle"
+                console.log("1" + theirType)
+            } else if(obj.hasOwnProperty('passengers')){
+                dataObjects = obj.passengers
+                myType = "vehicle"
+                theirType = "passenger"
+            }
+            parseJson();
+        }
+    }
+    http.send(data);
+}
+
+function parseJson() {
+
 }
